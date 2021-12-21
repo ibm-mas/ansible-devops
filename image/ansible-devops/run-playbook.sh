98b4c2a6781ed354af5ce43fcce39019897e3d87
Alexandre Quinteiro <alefq@br.ibm.com>
2021-12-21 01:37:48 -0300

    Refactor

--- image/ansible-devops/run-playbook.sh
+++ image/ansible-devops/run-playbook.sh
@@ -9,18 +9,7 @@ if [ -e "/workspace/entitlement/entitlement.lic" ]; then
 fi
 
 source /opt/app-root/src/env.sh
-
-#-Temporary (Test Only)
-echo "ibmcloud cli - installing container-service plugin"
-ibmcloud plugin install container-service
-echo "ibmcloud cli - list plugins"
-ibmcloud plugin list
-echo "ibmcloud cli - login"
-echo ${IBMCLOUD_APIKEY}
-ibmcloud login --apikey ${IBMCLOUD_APIKEY} --no-region --quiet
-echo "ibmcloud cli - end"
-#----------------------
-
+./ibmcloud-oc.sh
 ansible-playbook "$@"
 rc=$?
 python3 /opt/app-root/src/save-junit-to-mongo.py
