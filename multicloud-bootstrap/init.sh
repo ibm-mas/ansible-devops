ca7a0284de21302864fba7b07c469eb3d2e5b1d2
Shubham-Malvankar <shubham.malvankar@ibm.com>
2022-04-11 18:06:02 +0530

    Added missing var mas_appws_jdbc_binding in the playbook configure-app.yml and changed init.sh

--- multicloud-bootstrap/init.sh
+++ multicloud-bootstrap/init.sh
@@ -165,6 +165,7 @@ export MAS_WORKSPACE_ID="wsmasocp"
 export MAS_WORKSPACE_NAME="wsmasocp"
 export MAS_CONFIG_SCOPE="wsapp"
 export MAS_APP_ID=manage
+export MAS_APPWS_JDBC_BINDING="workspace-application"
 export MAS_JDBC_CERT_LOCAL_FILE=$GIT_REPO_HOME/db.crt
 export MAS_CLOUD_AUTOMATION_VERSION=1.0
 
