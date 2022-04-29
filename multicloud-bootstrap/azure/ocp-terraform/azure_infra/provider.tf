2342a90d2a8678c4a3bb7f79b30488bc4ba36a0b
David Parker <parkerda@uk.ibm.com>
2022-04-29 14:06:59 +0100

    [patch] Remove multicloud-bootstrap
    Moved to https://github.com/ibm-mas/multicloud-bootstrap

--- multicloud-bootstrap/azure/ocp-terraform/azure_infra/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  version         = "= 2.6.0"
-  subscription_id = var.azure-subscription-id
-  client_id       = var.azure-client-id
-  client_secret   = var.azure-client-secret
-  tenant_id       = var.azure-tenant-id
-  features {}
-}
