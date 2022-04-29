6dd809973b97e6334d5e4553a9fb72914e060475
David Parker <parkerda@uk.ibm.com>
2022-04-29 18:11:15 +0100

    [patch] Remove ibmcloud-oc login hack (#284)

--- image/ansible-devops/ibmcloud-oc.sh
+++
@@ -1,8 +0,0 @@
-# Install Plugin
-echo "Install IBM Cloud plugin"
-ibmcloud plugin install container-service
-ibmcloud plugin list
-
-# Authenticate
-echo "Authenticate in IBM Cloud"
-ibmcloud login --apikey ${IBMCLOUD_APIKEY} --no-region --quiet
