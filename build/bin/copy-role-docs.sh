2ecdf57ff61d9db96012bc7f99badbc21b6a7a8f
André Marcelino <31037381+andrercm@users.noreply.github.com>
2023-05-25 05:50:05 -0300

    [minor] Add support for AWS route53 as DNS provider for MAS installs (#828)

--- build/bin/copy-role-docs.sh
+++ build/bin/copy-role-docs.sh
@@ -14,6 +14,7 @@ copyDoc ansible_version_check
 copyDoc aws_bucket_access_point
 copyDoc aws_documentdb_user
 copyDoc aws_policy
+copyDoc aws_route53
 copyDoc aws_user_creation
 copyDoc aws_vpc
 copyDoc appconnect
