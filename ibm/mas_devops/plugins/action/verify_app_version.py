9272442511e4344641c8a916c937ea9d7d6cc02c
Sekhar C Valluri <svalluri@in.ibm.com>
2023-08-17 13:24:03 +0530

    [patch] fix plugin syntax issue

--- ibm/mas_devops/plugins/action/verify_app_version.py
+++ ibm/mas_devops/plugins/action/verify_app_version.py
@@ -23,36 +23,39 @@ class ActionModule(ActionBase):
 
         display.v(f"Checking {mas_app_id} version is matching ({retries} retries with a {delay} second delay)")
 
-        masAppInstances = ""
-        if mas_app_id == "manage"
+        masAppInstances = None
+        if mas_app_id == "manage":
           masAppInstances = dynaClient.resources.get(api_version="apps.mas.ibm.com/v1", kind='ManageApp')
 
-        versionMatching = False
-        version = ""
-        attempts = 0
+        if masAppInstances is not None:
+          versionMatching = False
+          version = ""
+          attempts = 0
 
-        while attempts < retries and not versionMatching:
+          while attempts < retries and not versionMatching:
 
-          appInstances = masAppInstances.get()
-          attempts += 1
+            appInstances = masAppInstances.get()
+            attempts += 1
 
-          for appInstance in appInstances.items:
-            message = f"{appInstance.metadata.namespace}/{appInstance.metadata.name} = {appInstance.status.versions.reconciled}"
-            display.v(f"* {message}")
-            if appInstance.metadata.name == mas_instance and appInstance.status.versions.reconciled == mas_app_version:
-              versionMatching = True
-              break
+            for appInstance in appInstances.items:
+              message = f"{appInstance.metadata.namespace}/{appInstance.metadata.name} = {appInstance.status.versions.reconciled}"
+              display.v(f"* {message}")
+              if appInstance.metadata.name == mas_instance and appInstance.status.versions.reconciled == mas_app_version:
+                versionMatching = True
+                break
 
-          if not versionMatching and attempts < retries:
-            display.v(f"Delaying {delay} seconds before next check")
-            time.sleep(delay)
+            if not versionMatching and attempts < retries:
+              display.v(f"Delaying {delay} seconds before next check")
+              time.sleep(delay)
 
-        if versionMatching:
-          return dict(
-            message="MAS {mas_app_id} version is matched",
-            failed=False,
-            changed=False,
-            version=mas_app_version
-          )
+          if versionMatching:
+            return dict(
+              message=f"MAS {mas_app_id} version is matched",
+              failed=False,
+              changed=False,
+              version=mas_app_version
+            )
+          else:
+            raise AnsibleError(f"Error: MAS {mas_app_id} version is not matched")
         else:
-          raise AnsibleError(f"Error: MAS {mas_app_id} version is not matched")
+            raise AnsibleError(f"Error: MAS ({mas_app_id}) instance doesn't exist")
