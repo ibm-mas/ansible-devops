48be8ae27cee4b73bc58509838afe575dc4aecfd
whitfiea <whitfiea@uk.ibm.com>
2024-10-21 17:21:56 +0100

    use gh action

--- build/bin/build-execution-environment.sh
+++ build/bin/build-execution-environment.sh
@@ -4,7 +4,6 @@ set -e
 if [ "$DEV_MODE" != "true" ]; then
   source ${GITHUB_WORKSPACE}/build/bin/.env.sh
   source ${GITHUB_WORKSPACE}/build/bin/.functions.sh
-  install_yq_farah
   install_ansible_builder
 fi
 
