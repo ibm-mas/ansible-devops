07d0962a6a2ec6d06557817df8f84869d59ce000
David Parker <parkerda@uk.ibm.com>
2025-02-06 17:51:52 +0000

    [patch] Update Ansible dependencies (#1647)

--- ibm/mas_devops/plugins/action/verify_subscriptions.py
+++ ibm/mas_devops/plugins/action/verify_subscriptions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from ansible_collections.kubernetes.core.plugins.module_utils.common import get_api_client
+from ansible_collections.kubernetes.core.plugins.module_utils.k8s.client import get_api_client
 
 from ansible.plugins.action import ActionBase
 from ansible.errors import AnsibleError
