bf14e633930b2faa6a2c9fcd397513bb82d13a77
Anil Prajapati <Anil.Prajapati@ibm.com>
2025-09-02 12:34:44 +0530

    [minor] add 2nd sep catalog

--- build/bin/build-collection.sh
+++ build/bin/build-collection.sh
@@ -13,8 +13,8 @@ cat $GITHUB_WORKSPACE/ibm/mas_devops/galaxy.yml
 
 
 # Update this when we have new catalog
-MAS_PREVIOUS_CATALOG='v9-250624-amd64'
-MAS_LATEST_CATALOG='v9-250731-amd64'
+MAS_PREVIOUS_CATALOG='v9-250828-amd64'
+MAS_LATEST_CATALOG='v9-250902-amd64'
 
 
 # Update all the placeholders in the playbooks
