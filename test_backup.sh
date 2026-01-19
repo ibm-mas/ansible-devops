0e365a684cbab686338fa39e1102e2638d59c581
whitfiea <whitfiea@uk.ibm.com>
2026-01-19 14:58:50 +0000

    remove files

--- test_backup.sh
+++
@@ -1,7 +0,0 @@
-export MAS_INSTANCE_ID=backup
-export MAS_BACKUP_DIR=/Users/whitfiea/temp/mas_backups
-export ROLE_NAME=suite_backup
-
-export SUITE_BACKUP_VERSION=260119-112418
-
-/Users/whitfiea/Work/Git/ibm-mas/ansible-devops/.venv/bin/ansible-playbook  ibm.mas_devops.run_role -vvv 
--- test_restore.sh
+++
@@ -1,9 +0,0 @@
-export MAS_INSTANCE_ID=backup
-export MAS_BACKUP_DIR=/Users/whitfiea/temp/mas_backups
-export ROLE_NAME=suite_restore
-
-export SUITE_BACKUP_VERSION=260119-112418
-
-export MAS_DOMAIN=backup.apps.brtest.cp.fyre.ibm.com
-
-/Users/whitfiea/Work/Git/ibm-mas/ansible-devops/.venv/bin/ansible-playbook  ibm.mas_devops.run_role -vvv 
