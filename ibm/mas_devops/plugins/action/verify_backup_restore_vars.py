5b8e24c5b8677ebffc4e7e8834e5d38ba6b920e2
Sanjay Prabhakar <sanjay.prabhakar@uk.ibm.com>
2026-01-21 09:50:35 +0000

    [patch] restore catalogs

--- ibm/mas_devops/plugins/action/verify_backup_restore_vars.py
+++ ibm/mas_devops/plugins/action/verify_backup_restore_vars.py
@@ -8,7 +8,8 @@ class ActionModule(ActionBase):
 
     REQUIRED = {
         "catalog": {
-            "backup": ["mas_backup_dir"]
+            "backup": ["mas_backup_dir"],
+            "restore": ["mas_backup_dir", "catalog_backup_version"]
         },
         "certmanager": {
             "backup": ["mas_backup_dir"]
